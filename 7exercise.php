2cfbcf6da972c73d5f648fb36fa7aa2e8614f169
jtamulaityte <jonesiuksliadeze@gmail.com>
2022-11-28 20:47:22 +0200

    pakeista uzduotis

--- 7exercise.php
+++ 7exercise.php
@@ -1,7 +1,7 @@
 <!--
 // PAPILDOMAS
 //  7. Parašykite programą, kuri sugeneruotų ornamentą: https://iili.io/H3J974e.png . 
-// Ornamentas turi būti sugeneruotas naudojant HTML ir PHP. (2 balai)
+// Ornamentas turi būti sugeneruotas naudojant HTML ir PHP. 
 -->
 
 <html>
